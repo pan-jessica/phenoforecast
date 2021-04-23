096ecec09eef2e5fde7c87d40a722c0972298c8d
yiluansong <yiluansong@gmail.com>
2021-04-23 22:23:03 +0000

    store files locally

--- copyfiles.R
+++ copyfiles.R
@@ -1,6 +1,14 @@
 library(R.utils)
 path_data<-paste0("/srv/shiny-server/phenoforecast_shinyapp/vmdata/archive/",today,"/")
 path_shiny<-("/srv/shiny-server/phenoforecast_shinyapp")
+
+genusoi_list <- c(
+  "Quercus", 
+  "Betula",
+  "Populus",
+  "Acer"
+)
+
 for (i in 1:length(genusoi_list)){
   copyDirectory(from=paste0(path_data,genusoi, "leaf/output/maps"), to=paste0(path_shiny,"/",genusoi, "/leaf"))
   copyDirectory(from=paste0(path_data,genusoi, "flower/output/maps"), to=paste0(path_shiny,"/",genusoi, "/flower"))
