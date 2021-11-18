dd65d4b85f9e84f5eebb43afaef80fe1f2899513
yiluansong <yiluansong@gmail.com>
2021-11-18 02:17:06 -0800

    update to R Open

--- updateapp.sh
+++ updateapp.sh
@@ -5,5 +5,5 @@ sudo git pull
 NEW_HEAD=$(sudo git rev-parse HEAD)
 if [ $OLD_HEAD != $NEW_HEAD ]
 then
-  sudo opt/microsoft/ropen/4.0.2/lib64/R/bin/Rscript "/srv/shiny-server/phenoforecast/copyfiles.R"
+  /opt/microsoft/ropen/4.0.2/lib64/R/bin/Rscript "/srv/shiny-server/phenoforecast/copyfiles.R"
 fi
